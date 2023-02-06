3f3b140416626346fa158da21b496a0805c6f607
Daniel Neukirch Hansen <danielnh@stud.ntnu.no>
2023-02-06 18:35:15 +0100

    Fix: Restructured file, add test code
    Changed the file format and added some dummy code for testing
    
    @DanielNHansen

---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCTdBw2BoB7wgu97Vaq5MODI_m23ArJ2yY",
+    authDomain: "prometheus-7165f.firebaseapp.com",
+    projectId: "prometheus-7165f",
+    storageBucket: "prometheus-7165f.appspot.com",
+    messagingSenderId: "191458230726",
+    appId: "1:191458230726:web:a2dde77b002871e74bf2c7",
+    measurementId: "G-CTJYXCGM83"
+})
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
---
+++ src/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+//Test code under here by Daniel :)
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { seconds: 0 };
+  }
+
+  tick() {
+    this.setState(state => ({
+      seconds: state.seconds + 1
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div>
+        Seconds: {this.state.seconds}
+      </div>
+    );
+  }
+}
+
+root.render(<Timer />);
+//Test code over here by Daniel:)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
