6cbe9d95d902de2f4e3752086238debc726c7003
hennidr <hennidr@stud.ntnu.no>
2023-02-13 12:52:19 +0100

    feat: :fire: lage sider for oversikt over annonser
    #8

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
+import AllListings from './AllListings';
 import './App.css';
+import MyListings from './MyListings';
+import ResponsiveAppBar from './ResponsiveAppBar';
+
+
+
 
 function App() {
 
   return (
-    <div className="App">
+    <div>
+      
+      <ResponsiveAppBar>
+      </ResponsiveAppBar>  
+      <Routes>
+        <Route path="/" element={<AllListings/>}/>
+        <Route path="/Mine Annonser" element={<MyListings/>}/>
+      
+      </Routes>  
 
+    
+
+      
     </div>
+
+  
+    
   );
 
 }
