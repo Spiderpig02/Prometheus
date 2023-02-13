edb4d6e943075241e7d43d5bce20395b8d9698e3
Oliver Bekkeheien Nesvik <oliverne@stud.ntnu.no>
2023-02-13 14:21:16 +0100

    Ferdig funksjonalitet for å hente ads fra userID

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
 import './App.css';
+import { useInfoFromUser } from './IO';
+import { getAdsFromUser } from './IO';
 
 function App() {
 
+  const user = useInfoFromUser();
+  const ad = getAdsFromUser("Askeladden"); 
+
   return (
     <div className="App">
-
+      {ad.map((ad) => {
+        return <div>
+          {" "}
+          <h1> UserID: {ad.userID} </h1>
+          <h1> Title: {ad.Title} </h1>
+          <h1> Beskrivelse: {ad.Beskrivelse} </h1>
+          </div>
+      })}
     </div>
   );
 
--- src/index.js
+++ src/index.js
@@ -11,39 +11,6 @@ root.render(
   </React.StrictMode>
 );
 
-//Test code under here by Daniel :)
-class Timer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { seconds: 0 };
-  }
-
-  tick() {
-    this.setState(state => ({
-      seconds: state.seconds + 1
-    }));
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  render() {
-    return (
-      <div>
-        Seconds: {this.state.seconds}
-      </div>
-    );
-  }
-}
-
-root.render(<Timer />);
-//Test code over here by Daniel:)
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
