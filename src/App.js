bd689d5fac41e2769c02997f8a60867f4e178ac5
Mikkel Emil Lange Friis <mefriis@stud.ntnu.no>
2023-02-13 14:43:13 +0100

    fix: :bug: Add imports to App.js
    To eliminate errors
    
    #19 Co-authored-by solvhold solvhold@stud.ntnu.no

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import './App.css';
 import { useInfoFromUser } from './IO';
 import { getAdsFromUser } from './IO';
+import React from 'react';
+import { Route, Routes } from 'react-router';
+import AllListings from './AllListings';
+import './App.css';
+import MyListings from './MyListings';
+import ResponsiveAppBar from './ResponsiveAppBar';
 
 function App() {
 
-  const user = useInfoFromUser();
-  const ad = getAdsFromUser("Askeladden"); 
+    const user = useInfoFromUser();
+    const ad = getAdsFromUser("Askeladden");
 
-  return (
-    <div>
-      
-      <ResponsiveAppBar>
-      </ResponsiveAppBar>  
-      <Routes>
-        <Route path="/" element={<AllListings/>}/>
-        <Route path="/Mine Annonser" element={<MyListings/>}/>
-        <Route path="/Alle Annonser" element={<AllListings/>}/>
-      
-      </Routes>  
+    return (
+        <div>
 
-    
+            <ResponsiveAppBar>
+            </ResponsiveAppBar>
 
-      
-    </div>
+            <Routes>
+                <Route path="/" element={<AllListings />} />
+                <Route path="/Mine Annonser" element={<MyListings />} />
+                <Route path="/Alle Annonser" element={<AllListings />} />
 
-  
-    
-  );
+            </Routes>
+
+
+
+
+        </div>
+
+
+
+    );
 
 }
 
