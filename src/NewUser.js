0be3eae7dec645508a213578691d54cda82c2926
Oliver Bekkeheien Nesvik <oliverne@stud.ntnu.no>
2023-02-20 15:02:45 +0100

    Funksjonalitet som registrer nye brukere

--- src/NewUser.js
+++ src/NewUser.js
@@ -1,20 +1,39 @@
 import React from "react"
 import NewUser from "./NewUser.css"
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
 
 function RegisterNewUser() {
+
+    const auth = getAuth()
+
+    const handleSubmit = (submit) => {
+        submit.preventDefault()
+
+        const email = submit.target.emailInput.value
+        const password = submit.target.passwordInput.value
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((cred) => {
+                console.log("User created", cred.user)
+                
+            })
+            .catch((e) => {
+                console.log(e.message)
+            })
+    }
 
     return (
        
         <div className= "ny-bruker-form">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1> Registrer ny bruker </h1>
                 <div>
-                    <label htmlFor="e-post"> E-post (ditt brukernavn): </label>
-                    <input type="text" placeholder="E-post" required /> 
+                    <label htmlFor="e-post"> E-post: </label>
+                    <input type="text" placeholder="E-post" name="emailInput" required /> 
                 </div>
                 <div>
                     <label htmlFor="passord"> Passord: </label>
-                    <input type="text" placeholder="Passord" required /> 
+                    <input type="text" placeholder="Passord" name="passwordInput" required /> 
                 </div>
                     <button class="shadow"> Registrer deg! </button>
             </form>
