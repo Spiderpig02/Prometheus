930c2869b2fa39ee153075727d00db0a2618cfe5
solvhold <solvhold@stud.ntnu.no>
2023-02-21 22:44:37 +0100

    feat: :white_check_mark: test render of LagAnnonse
    to check if LagAnnonse render correctly
    
    #47

---
+++ src/LagAnnonse.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import LagAnnonse from './LagAnnonse'
+
+  describe("Test Lag Annonse", () => {
+    it("should render LagAnnonse correctly", () => {
+      render(<LagAnnonse/>);
+      expect(screen.getByRole("heading")).toBeInTheDocument();
+      expect(screen.getByRole("textbox", { name: "Tittel:" })).toBeInTheDocument();
+      expect(screen.getByRole("textbox", { name: "Beskrivelse:" })).toBeInTheDocument();
+      expect(screen.getByRole("radio", { name: "Annonse" })).toBeInTheDocument();
+      expect(screen.getByRole("radio", { name: "Etterspørsel" })).toBeInTheDocument();
+      expect(screen.getByRole("button", { name: "Lag annonse eller etterspørsel" })).toBeInTheDocument();
+    });
+
+  })
+
