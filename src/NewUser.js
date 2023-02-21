359181492367dd440a7ede5d88183bb697472aad
Mikkel Emil Lange Friis <mefriis@stud.ntnu.no>
2023-02-21 10:40:05 +0100

    style: :lipstick: Add import of NewUser.css
    To fix styling
    
    #48

--- src/NewUser.js
+++ src/NewUser.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
 import { addUser } from './IO.js'
+import './NewUser.css';
 
 function RegisterNewUser() {
 
@@ -25,26 +26,26 @@ function RegisterNewUser() {
     }
 
     return (
-        <div className= "ny-bruker-form">
+        <div className="ny-bruker-form">
             <form onSubmit={handleSubmit}>
                 <h1> Registrer ny bruker </h1>
                 <div>
                     <label htmlFor="e-post"> E-post: </label>
-                    <input type="text" placeholder="E-post" name="emailInput" required /> 
+                    <input type="text" placeholder="E-post" name="emailInput" required />
                 </div>
                 <div>
                     <label htmlFor="brukernavn"> Brukernavn: </label>
-                    <input type="text" placeholder="Brukernavn" name="usernameInput" required /> 
+                    <input type="text" placeholder="Brukernavn" name="usernameInput" required />
                 </div>
                 <div>
                     <label htmlFor="telefon-nummer"> Telefon nummer: </label>
-                    <input type="text" placeholder="Telefon nummer" name="phonenumberInput" required /> 
+                    <input type="text" placeholder="Telefon nummer" name="phonenumberInput" required />
                 </div>
                 <div>
                     <label htmlFor="passord"> Passord: </label>
-                    <input type="text" placeholder="Passord" name="passwordInput" required /> 
+                    <input type="text" placeholder="Passord" name="passwordInput" required />
                 </div>
-                    <button class="shadow"> Registrer deg! </button>
+                <button class="shadow"> Registrer deg! </button>
             </form>
         </div>
     )
