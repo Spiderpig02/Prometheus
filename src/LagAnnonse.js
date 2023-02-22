6a044c5f57d64c7480616720889fd65b4ec593d2
solvhold <solvhold@stud.ntnu.no>
2023-02-22 20:24:49 +0100

    feat: :sparkles: go to login if not logged in
    when going to "Lag annonse" navigate to login if not logged in, else go to "Lag annonse"
    
    #59

--- src/LagAnnonse.js
+++ src/LagAnnonse.js
@@ -3,6 +3,7 @@ import { Timestamp } from 'firebase/firestore';
 import React, { useState } from 'react'
 import { auth } from './firebaseConfig';
 import { addAd } from './IO'
+import { Navigate } from "react-router";
 import './LagAnnonse.css'
 
 function LagAnnonse() {
@@ -46,69 +47,76 @@ function LagAnnonse() {
     // Return classes based on whether item is checked
     var isChecked = (item) =>
     checked.includes(item) ? "checked-item" : "not-checked-item";
-    return (
-        <div className='form-content'>
-            <h1>Lag annonse eller etterspørsel</h1>
-            <form onSubmit={submit}>
-                <div className='tittel inputElement flexboks'>
-                    <label htmlFor="tittel">Tittel:</label>
-                    <input className="testBox" placeholder='Tittel' name='tittel' id='tittel' type="text" value={title} onChange={(event) => setTitle(event.target.value)} required />
-                </div>
 
-                <div className='beskrivelse inputElement flexboks'>
-                    <label htmlFor="beskrivelse">Beskrivelse:</label>
-                    <textarea className="testBox" placeholder='Beskrivelse' id="beskrivelse" rows={5} value={description} onChange={(event) => setDescription(event.target.value)} required></textarea>
-                </div>
-
-
-                <label htmlFor="type"> Type:</label>
-               <div className='checkboxes'>
-                    <h3>Velg ønskede kategorier</h3> 
-                    <div className="list-container">
-                            {checkList.map((item, index) => (
-                            <div key={index}>
-                                <input value={item} type="checkbox" onChange={handleCheck} />
-                                <span className={isChecked(item)}>{item}</span>
+    if(userData !== null){
+        return (
+            <div className='form-content'>
+                <h1>Lag annonse eller etterspørsel</h1>
+                <form onSubmit={submit}>
+                    <div className='tittel inputElement flexboks'>
+                        <label htmlFor="tittel">Tittel:</label>
+                        <input className="testBox" placeholder='Tittel' name='tittel' id='tittel' type="text" value={title} onChange={(event) => setTitle(event.target.value)} required />
+                    </div>
+    
+                    <div className='beskrivelse inputElement flexboks'>
+                        <label htmlFor="beskrivelse">Beskrivelse:</label>
+                        <textarea className="testBox" placeholder='Beskrivelse' id="beskrivelse" rows={5} value={description} onChange={(event) => setDescription(event.target.value)} required></textarea>
+                    </div>
+    
+    
+                    <label htmlFor="type"> Type:</label>
+                   <div className='checkboxes'>
+                        <h3>Velg ønskede kategorier</h3> 
+                        <div className="list-container">
+                                {checkList.map((item, index) => (
+                                <div key={index}>
+                                    <input value={item} type="checkbox" onChange={handleCheck} />
+                                    <span className={isChecked(item)}>{item}</span>
+                                </div>
+                                ))}
                             </div>
-                            ))}
-                        </div>
-                    {/* <h3>Velg ønskede kategorier</h3>
-                        <div className='sidebar-checkbox'>
-                            <input id="verktøyDiverse" type="checkbox" value={checked} onChange={onChange}/>
-                            <label htmlFor="verktøyDiverse">Diverse</label>         
-                        </div>
-                        <div className='sidebar-checkbox'>
-                            <input id="verktøyHage" type="checkbox" value={checked} onChange={onChange}/>
-                            <label htmlFor="verktøyHage">Hageverktøy</label>  
-                        </div>
-                        <div className='sidebar-checkbox'>
-                            <input id="verktøyMaling" type="checkbox" value={checked} onChange={onChange}/>
-                            <label htmlFor="verktøyMaling">Maleverktøy</label>  
-                        </div>
-                        <div className='sidebar-checkbox'>
-                            <input id="verktøySnekring" type="checkbox" value={checked} onChange={onChange}/>
-                            <label htmlFor="verktøySnekring">Snekring</label>  
-                        </div>
-                        <div className='sidebar-checkbox'>
-                            <input id="verktøyFritid" type="checkbox" value={checked} onChange={onChange}/>
-                            <label htmlFor="verktøyFritid">Fritidsverktøy</label>  
-                    </div>   */}
-               </div>
-                <div className='inputElement flexboks'>
-                    <label htmlFor="annonse">Annonse</label>
-                    <input type="radio" value="Annonse" id='annonse' onClick={() => setType('Annonse')} /> 
-                    
-
-                    <label htmlFor="etterspørsel">Etterspørsel</label>
-                    <input type="radio" value="Etterspørsel" id='etterspørsel' onClick={() => setType('Etterspørsel')} />
-                    
-
-                </div>
-
-                <button type='submit'>Lag annonse eller etterspørsel</button>
-            </form>
-        </div>
-    )
+                        {/* <h3>Velg ønskede kategorier</h3>
+                            <div className='sidebar-checkbox'>
+                                <input id="verktøyDiverse" type="checkbox" value={checked} onChange={onChange}/>
+                                <label htmlFor="verktøyDiverse">Diverse</label>         
+                            </div>
+                            <div className='sidebar-checkbox'>
+                                <input id="verktøyHage" type="checkbox" value={checked} onChange={onChange}/>
+                                <label htmlFor="verktøyHage">Hageverktøy</label>  
+                            </div>
+                            <div className='sidebar-checkbox'>
+                                <input id="verktøyMaling" type="checkbox" value={checked} onChange={onChange}/>
+                                <label htmlFor="verktøyMaling">Maleverktøy</label>  
+                            </div>
+                            <div className='sidebar-checkbox'>
+                                <input id="verktøySnekring" type="checkbox" value={checked} onChange={onChange}/>
+                                <label htmlFor="verktøySnekring">Snekring</label>  
+                            </div>
+                            <div className='sidebar-checkbox'>
+                                <input id="verktøyFritid" type="checkbox" value={checked} onChange={onChange}/>
+                                <label htmlFor="verktøyFritid">Fritidsverktøy</label>  
+                        </div>   */}
+                   </div>
+                    <div className='inputElement flexboks'>
+                        <label htmlFor="annonse">Annonse</label>
+                        <input type="radio" value="Annonse" id='annonse' onClick={() => setType('Annonse')} /> 
+                        
+    
+                        <label htmlFor="etterspørsel">Etterspørsel</label>
+                        <input type="radio" value="Etterspørsel" id='etterspørsel' onClick={() => setType('Etterspørsel')} />
+                        
+    
+                    </div>
+    
+                    <button type='submit'>Lag annonse eller etterspørsel</button>
+                </form>
+            </div>
+        )
+    }
+    else{
+        return <Navigate replace to = "/Logg inn"></Navigate>
+    }
+    
 }
 
 export default LagAnnonse
