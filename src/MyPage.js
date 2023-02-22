3b3f606a003762d6fe7a4b7927c4363858a8fc11
solvhold <solvhold@stud.ntnu.no>
2023-02-22 20:15:57 +0100

    feat: :sparkles: add functionality for navigating to log in page when not logged in
    if not logged in user is sent to log in when trying to "Min Profil"
    
    #26

--- src/MyPage.js
+++ src/MyPage.js
@@ -1,29 +1,41 @@
 import { Container, Paper, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { Navigate } from "react-router";
+import { getAuth } from "firebase/auth";
 
 const MyPage = () => {
-    return ( 
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+
+    if(user !== null){
+        return ( 
     
-    <Container style={{marginTop: '100px'}}>
-        <Typography variant='h3' sx={{ my: 4, textAlign: 'center', color: "primary.main" }}>
-        Min Side
-            <Paper>
-                <h3>
-                    Brukernavn: 
-                </h3>
-                <h3>
-                    Min Rating: 
-                </h3>
-                <h3>
-                    Annen Statistikk: 
-                </h3>
-                
-            </Paper>
-        </Typography>
-    </Container>
+            <Container style={{marginTop: '100px'}}>
+                <Typography variant='h3' sx={{ my: 4, textAlign: 'center', color: "primary.main" }}>
+                Min Side
+                    <Paper>
+                        <h3>
+                            Brukernavn: 
+                        </h3>
+                        <h3>
+                            Min Rating: 
+                        </h3>
+                        <h3>
+                            Annen Statistikk: 
+                        </h3>
+                        
+                    </Paper>
+                </Typography>
+            </Container>
+            
+           
+             );
+    }
+    else{
+        return <Navigate replace to = "/Logg inn"></Navigate>
+    }
     
-   
-     );
 }
  
 export default MyPage;
