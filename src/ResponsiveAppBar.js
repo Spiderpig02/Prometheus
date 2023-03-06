84700b68b2d44025507dce156cf7e8b85573b3ef
Mikkel Emil Lange Friis <mefriis@stud.ntnu.no>
2023-03-06 14:23:53 +0100

    feat: :sparkles: Add button to navbar and method to change class of body
    To enable darkmode/lightmode
    
    #76

--- src/ResponsiveAppBar.js
+++ src/ResponsiveAppBar.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -12,150 +13,175 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import ConstructionIcon from '@mui/icons-material/Construction';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import toggleThemeMode from "./App";
+
 const pages = ['Alle Annonser', 'Mine Annonser', "Lag Annonse", "Logg inn"];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
 
-  return (
-    <AppBar position="fixed">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <ConstructionIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.1rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Tors Hammer Utleieservice    |
-          </Typography>
+    const handleViewModeSwitch = () => {
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">
-                    <Link style={{ textDecoration:"none", color:"whitesmoke" }} to={`/${page}`} >
-                      {page}  
-                    </Link>
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <ConstructionIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-            //Under her kommer small
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                <Link style={{ textDecoration:"none", color:"whitesmoke" }} to={`/${page}`} >
-                      {page}  
-                    </Link>
-              </Button>
-            ))}
-            
-          </Box>
+        if (document.body.classList.contains('lightMode')) {
+            console.log("Jai")
+            document.body.className = "darkMode";
+            document.getElementById("pageWrapper").className = "App darkMode"
 
-          <Box sx={{ flexGrow: 0 }}>
+        } else if (document.body.classList.contains('darkMode')) {
+            console.log("Nai")
+            document.body.className = "lightMode";
+            document.getElementById("pageWrapper").className = "App lightMode"
+        }
+    };
 
 
-        
+    return (
+        <AppBar position="fixed">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <ConstructionIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.1rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Tors Hammer Utleieservice    |
+                    </Typography>
 
-          
-            <Tooltip title="Brukerinnstillinger">
-              <Button onClick={handleOpenUserMenu} sx={{ p: 0 }}
-              
-              >
-                <Link style={{ textDecoration:"none", color:"whitesmoke" }} to={`/Min Profil`} >
-                      {"Min Profil"}  
-                    </Link>
-                
-      
-                
-              </Button>
-            </Tooltip>
-            
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">
+                                        <Link style={{ textDecoration: "none", color: "whitesmoke" }} to={`/${page}`} >
+                                            {page}
+                                        </Link>
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <ConstructionIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    //Under her kommer small
+                    >
+                        LOGO
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                <Link style={{ textDecoration: "none", color: "whitesmoke" }} to={`/${page}`} >
+                                    {page}
+                                </Link>
+                            </Button>
+                        ))}
+
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+
+
+
+                        <Button onClick={handleViewModeSwitch}>
+                            <p style={{
+                                color: "whitesmoke", border: "1px black solid",
+                                padding: "5px", borderRadius: "10px", backgroundColor: "gray"
+                            }}>
+                                {"View Mode"}</p>
+                        </Button>
+
+
+
+                        <Tooltip title="Brukerinnstillinger">
+                            <Button onClick={handleOpenUserMenu} sx={{ p: 0 }}
+
+                            >
+                                <Link style={{ textDecoration: "none", color: "whitesmoke" }} to={`/Min Profil`} >
+                                    {"Min Profil"}
+                                </Link>
+
+
+
+                            </Button>
+                        </Tooltip>
+
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar >
+    );
 }
 export default ResponsiveAppBar;
