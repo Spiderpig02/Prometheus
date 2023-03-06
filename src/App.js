43a4994646c4ed14c3600b191dec525d11d841bf
Mikkel Emil Lange Friis <mefriis@stud.ntnu.no>
2023-03-06 12:38:10 +0100

    feat: :art: Add const for themeMode and toggleThemeMode, and useEffect on themeMode
    To add, edit and update themeMode
    
    #76

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router';
 import AllListings from './AllListings';
 import './App.css';
@@ -13,31 +13,34 @@ import RegisterNewUser from './NewUser';
 
 function App() {
 
-  return (
-    <div>
-      <ResponsiveAppBar>
-      </ResponsiveAppBar>
-      <Routes>
-        <Route path="/" element={<AllListings />} />
-        <Route path="/Mine Annonser" element={<MyListings />} />
-        <Route path="/Alle Annonser" element={<AllListings />} />
-        <Route path="/Lag Annonse" element={<LagAnnonse />} />
-        <Route path ="/Logg inn" element={<LoginUI />} />
-        <Route path ="/Min Profil" element={<MyPage/>} />
-        <Route path ="/Ny bruker side" element={<RegisterNewUser/>} />
+    // For dark/light mode
+    const [themeMode, setThemeMode] = useState('lightMode');
+    const toggleThemeMode = () => {
+        if (themeMode === 'lightMode') {
+            setThemeMode('darkMode');
+        } else {
+            setThemeMode('lightMode');
+        }
+    };
+    useEffect(() => {
+        document.body.className = themeMode;
+    }, [themeMode]);
 
-
-      </Routes>
-
-
-
-
-    </div>
-
-
-
-  );
-
+    return (
+        <div className={`App ${themeMode}`}>
+            <ResponsiveAppBar>
+            </ResponsiveAppBar>
+            <Routes>
+                <Route path="/" element={<AllListings />} />
+                <Route path="/Mine Annonser" element={<MyListings />} />
+                <Route path="/Alle Annonser" element={<AllListings />} />
+                <Route path="/Lag Annonse" element={<LagAnnonse />} />
+                <Route path="/Logg inn" element={<LoginUI />} />
+                <Route path="/Min Profil" element={<MyPage />} />
+                <Route path="/Ny bruker side" element={<RegisterNewUser />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
