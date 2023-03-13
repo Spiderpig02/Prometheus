bbbe970f4555eb61e77755d2f8c8e269597b50b2
Mikkel Emil Lange Friis <mefriis@stud.ntnu.no>
2023-03-13 10:31:50 +0100

    refactor: :recycle: Remove dead space
    To make ode more compact
    
    #81

--- src/AllListings.js
+++ src/AllListings.js
@@ -12,9 +12,6 @@ import OtherUser from "./OtherUser";
 
 export const AllListings = (props) => {
 
-
-   
-
     const [checkedList, setCheckedList] = useState([]);
     const [ads, setAds] = useState([]);
     const adsCollectionRef = collection(firestore, "Advertisement");
@@ -27,8 +24,6 @@ export const AllListings = (props) => {
             setAds(adsData);
         });
     }
-  
-    
 
     const getQueryAds = async () => {
         const querys = query(adsCollectionRef, where('Categories', 'array-contains-any', checkedList))
@@ -44,7 +39,6 @@ export const AllListings = (props) => {
         } else {
             getAds();
         }
-
     }, [checkedList, emptySearch]);
 
     const handleSetChecked = (checked) => {
@@ -59,7 +53,6 @@ export const AllListings = (props) => {
             if (adList[index].Title.toLowerCase().includes(searchText.toLowerCase())) {
                 newAdList.push(adList[index]);
             };
-
         };
         console.log(searchText);
         if (searchText.length === 0) {
@@ -70,84 +63,73 @@ export const AllListings = (props) => {
     };
 
     return (
-    <div>
-  
-        
-        <Container>
-            <Container className="ListingsContainer" sx={{ justifyContent: 'center', display: 'flex', padding: 0, paddingLeft: 0 }}>
-                <Box className='sidebar-container'>
-                    <CheckboxSidebar className="sidebar" onChecked={handleSetChecked} />
-                </Box>
-            </Container>
-            <Typography variant='h2' sx={{ my: 4, textAlign: 'center', color: "primary.main" }} className="pageHeading">
-                Alle Annonser
-            </Typography>
-            <div className="searchBar">
-                <input onChange={(event) => {
-                    filterBySearch();
-                }} type="text" id="searchField" name="searchField"
-                    placeholder="Søk etter annonse..."></input>
-                <button onClick={filterBySearch} id="searchFieldButton">Søk</button>
-            </div>
-
-            <List>
-
-            
-                      
-                    
-
-                {ads.map(ad => (
-                    <Box key={ad.id} sx={{
-                        
-                        //justifyContent: "space-between",
-                        margin: "30px",
-                        mx: 'auto',
-                        width: 700
-                    }}>
-                        <Paper elevation={3} style={{
-                            padding: 8,
-                            border: "1px solid black",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            textAlign: "center",
-                            verticalAlign: "middle"
-                        }}>
-                            <h4 className="addType">
-                                {ad.Type}
-                            </h4>
-                            <div className="paperTitleAndDate">
-                                <h1>
-                                    {ad.Title}
-                                </h1>
-                                <h4>
-                                    Dato opprettet: {new Date(ad.Created * 1000).toString().slice(3, 10)} 2023
-                                </h4>
-                            </div>
-                            <h3>
-                                {ad.Description}
-                            </h3>
-                            <a href={"https://www.google.com/maps/dir/?api=1&origin=&destination=" + ad.streetName.replace(/\s/g, '+') + "+" + ad.city.replace(/\s/g, '+') + "&travelmode=driving target=_blank"}>Veibeskrivelse</a>
-                            <h2>
-                                Kontakt: {ad.Phonenumber}
-                                
-                            </h2>
-
-                            <Link style={{ textDecoration:"none", color:"whitesmoke" }} onClick={() => props.recieveUser(ad.userID)} to={`/OtherUser`}  >
-                            <Button  variant="outlined">
-                                Se bruker sin side
-                                {/* useLocation for props gjennom link, mulig async? vet ikke  */}
-                            </Button>
-                            </Link>
-                     
-
-                        </Paper>
+        <div>
+            <Container>
+                <Container className="ListingsContainer" sx={{ justifyContent: 'center', display: 'flex', padding: 0, paddingLeft: 0 }}>
+                    <Box className='sidebar-container'>
+                        <CheckboxSidebar className="sidebar" onChecked={handleSetChecked} />
                     </Box>
-                ))}
+                </Container>
+                <Typography variant='h2' sx={{ my: 4, textAlign: 'center', color: "primary.main" }} className="pageHeading">
+                    Alle Annonser
+                </Typography>
+                <div className="searchBar">
+                    <input onChange={(event) => {
+                        filterBySearch();
+                    }} type="text" id="searchField" name="searchField"
+                        placeholder="Søk etter annonse..."></input>
+                    <button onClick={filterBySearch} id="searchFieldButton">Søk</button>
+                </div>
 
-            </List>
-        </Container>
+                <List>
+                    {ads.map(ad => (
+                        <Box key={ad.id} sx={{
 
-    </div>  
+                            //justifyContent: "space-between",
+                            margin: "30px",
+                            mx: 'auto',
+                            width: 700
+                        }}>
+                            <Paper elevation={3} style={{
+                                padding: 8,
+                                border: "1px solid black",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                textAlign: "center",
+                                verticalAlign: "middle"
+                            }}>
+                                <h4 className="addType">
+                                    {ad.Type}
+                                </h4>
+                                <div className="paperTitleAndDate">
+                                    <h1>
+                                        {ad.Title}
+                                    </h1>
+                                    <h4>
+                                        Dato opprettet: {new Date(ad.Created * 1000).toString().slice(3, 10)} 2023
+                                    </h4>
+                                </div>
+                                <h3>
+                                    {ad.Description}
+                                </h3>
+                                <a href={"https://www.google.com/maps/dir/?api=1&origin=&destination=" + ad.streetName.replace(/\s/g, '+') + "+" + ad.city.replace(/\s/g, '+') + "&travelmode=driving target=_blank"}>Veibeskrivelse</a>
+                                <h2>
+                                    Kontakt: {ad.Phonenumber}
+
+                                </h2>
+
+                                <Link style={{ textDecoration: "none", color: "whitesmoke" }} onClick={() => props.recieveUser(ad.userID)} to={`/OtherUser`}  >
+                                    <Button variant="outlined">
+                                        Se bruker sin side
+                                        {/* useLocation for props gjennom link, mulig async? vet ikke  */}
+                                    </Button>
+                                </Link>
+                            </Paper>
+                        </Box>
+                    ))}
+                </List>
+            </Container>
+        </div>
     );
 }
 
