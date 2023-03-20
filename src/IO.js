4b2ca0d184d8d7cf0c9787f5a94d4d086c219b03
solvhold <solvhold@stud.ntnu.no>
2023-03-20 22:35:02 +0100

    feat: :sparkles: add update interactions function
    #94 @oliverne

--- src/IO.js
+++ src/IO.js
@@ -1,16 +1,35 @@
 import { async } from "@firebase/util";
-import { doc, setDoc, addDoc, collection, updateDoc } from "firebase/firestore";
+import { doc, setDoc, addDoc, collection, updateDoc, getDoc } from "firebase/firestore";
 import { firestore } from "./firebaseConfig.js";
 
-export async function addUser(Uid, Username, Password, Email, Phonenumber, Rating, canRate, Blocked, Like) {
+export async function addUser(Uid, Username, Password, Email, Phonenumber, Rating, Interactions, Blocked, Like) {
     const usersDocRef = doc(firestore, "User", Uid);
     await setDoc(usersDocRef, {
-        Username, Password, Email, Phonenumber: Number(Phonenumber), Rating, canRate, Blocked, Like
+        Username, Password, Email, Phonenumber: Number(Phonenumber), Rating, Interactions, Blocked, Like 
     });
+};
+export async function updateUser(Interaction, id) {
+  const user = await getDoc(doc(firestore, "User", id)).then(res => {return res.data()});
+  let interactions = [];
+
+  if(user.Interactions !== undefined ){
+    interactions = user.Interactions.slice();
+    
+    if(!interactions.includes(Interaction)){  
+        interactions.push(Interaction)   
+    }
+  }
+ 
+  const updateField = doc(firestore, "User", id)
+        await updateDoc(updateField,
+            {
+                Interactions: interactions
+  });
+
+  
 };
 
 export async function addAd(Title, Description, userID, Phonenumber, Type, Categories, Created, streetName, city) {
-
     const addsCollectinRef = collection(firestore, "Advertisement");
     await addDoc(addsCollectinRef,
         {
