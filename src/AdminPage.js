ff9c3d48e125f89a33aa6d8d96bf486479c116f1
Mikkel Emil Lange Friis <mefriis@stud.ntnu.no>
2023-03-22 18:40:16 +0100

    refactor: :recycle: Change size of paper components
    To make room for button
    
    #88

--- src/AdminPage.js
+++ src/AdminPage.js
@@ -33,7 +33,7 @@ function AdminPage() {
         getUsers();
     }, []);
 
-    
+
 
     // const deleteAds = async (targetUserID) => {
     //     await getDocs(adsCollectionRef).then((querySnapshot) => {
@@ -50,122 +50,122 @@ function AdminPage() {
 
     const deleteAds = async (targetUserID) => {
         await getDocs(query(collection(firestore, "Advertisement"), where("userID", "==", targetUserID)))
-        .then((querySnapshot) => {
-            const targetAdsData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-            targetAdsData.map(async ad => {
-                await deleteDoc(doc(firestore, "Advertisement", ad.id));
+            .then((querySnapshot) => {
+                const targetAdsData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+                targetAdsData.map(async ad => {
+                    await deleteDoc(doc(firestore, "Advertisement", ad.id));
+                })
             })
-        })
     }
 
     return (
         <div>
             <h1 className="adminTitle">Velkommen, Admin!</h1>
             <Container class="container">
-            <div style={{display: "flex", justifyContent: "center"}}>
-            <div className="container-list" style={{display: "flex", width: "100%", maxWidth: "1400px"}}>
-            <List className="left-list" style={{flex:"1"}}>
-                <h2 className="listTitle">Brukere</h2>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <div className="container-list" style={{ display: "flex", width: "100%", maxWidth: "1400px" }}>
+                        <List className="left-list" style={{ flex: "1" }}>
+                            <h2 className="listTitle">Brukere</h2>
 
-                {users.map(user => (
+                            {users.map(user => (
 
-                <Box key={user.id} sx={{
+                                <Box key={user.id} sx={{
 
-                    //justifyContent: "space-between",
-                    margin: "30px",
-                    mx: 'auto',
-                    width: 700,
-                    height: "220px"
-                    }}>
+                                    //justifyContent: "space-between",
+                                    margin: "30px",
+                                    mx: 'auto',
+                                    width: 650,
+                                    height: "auto"
+                                }}>
 
-                    <Paper elevation={3} style={{
-                        padding: 8,
-                        border: "1px solid black",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        textAlign: "center",
-                        verticalAlign: "middle",
-                        height: "100%"
-                        }}>
-                        <h4 className="userName">
-                            {user.Username}
-                        </h4>
-                        <div className="userEmail">
-                            <h1>
-                                {user.Email}
-                            </h1>
-                        </div>
-                        <h3>
-                            {user.Phonenumber}
-                        </h3>
-                        <div className="advertPaperButtons">
-                            <button onClick={async () => {
-                                const userDoc = doc(firestore, "User", user.id);
-                                deleteAds(user.id);
-                                await deleteDoc(userDoc);
-                                getUsers();
-                                getAds();
-                            }}>Slett bruker</button>
-                        </div>
-                    </Paper>
-                </Box>
-                ))}
-            </List>
-            <div className="listSeparator"></div>
-            <List className="right-list" style={{flex:"1"}}>
-                <h2 className="listTitle">Annonser</h2>
-                {ads.map(ad => (
+                                    <Paper elevation={3} style={{
+                                        padding: 8,
+                                        border: "1px solid black",
+                                        justifyContent: "center",
+                                        alignItems: "center",
+                                        textAlign: "center",
+                                        verticalAlign: "middle",
+                                        height: "100%"
+                                    }}>
+                                        <h4 className="userName">
+                                            {user.Username}
+                                        </h4>
+                                        <div className="userEmail">
+                                            <h1>
+                                                {user.Email}
+                                            </h1>
+                                        </div>
+                                        <h3>
+                                            {user.Phonenumber}
+                                        </h3>
+                                        <div className="advertPaperButtons">
+                                            <button onClick={async () => {
+                                                const userDoc = doc(firestore, "User", user.id);
+                                                deleteAds(user.id);
+                                                await deleteDoc(userDoc);
+                                                getUsers();
+                                                getAds();
+                                            }}>Slett bruker</button>
+                                        </div>
+                                    </Paper>
+                                </Box>
+                            ))}
+                        </List>
+                        <div className="listSeparator"></div>
+                        <List className="right-list" style={{ flex: "1" }}>
+                            <h2 className="listTitle">Annonser</h2>
+                            {ads.map(ad => (
 
-                <Box key={ad.id} sx={{
+                                <Box key={ad.id} sx={{
 
-                    //justifyContent: "space-between",
-                    margin: "30px",
-                    mx: 'auto',
-                    width: 700,
-                    height: "220px"
-                    }}>
+                                    //justifyContent: "space-between",
+                                    margin: "30px",
+                                    mx: 'auto',
+                                    width: 650,
+                                    height: "auto"
+                                }}>
 
-                    <Paper elevation={3} style={{
-                        padding: 8,
-                        border: "1px solid black",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        textAlign: "center",
-                        verticalAlign: "middle",
-                        height: "100%"
-                        }}>
-                        <h4 className="addType">
-                            {ad.Type}
-                        </h4>
-                        <div className="paperTitleAndDate">
-                            <h1>
-                                {ad.Title}
-                            </h1>
-                            <h4>
-                                Dato opprettet: {new Date(ad.Created * 1000).toString().slice(3, 10)} 2023
-                            </h4>
-                        </div>
-                        <h3>
-                            {ad.Description}
-                        </h3>
-                        <div className="advertPaperButtons">
-                            <button onClick={async () => {
-                                const adDoc = doc(firestore, "Advertisement", ad.id);
-                                await deleteDoc(adDoc);
-                                getAds();
-                            }}>Slett annonse</button>
-                        </div>
-                    </Paper>
-                </Box>
-                    ))}
-                </List>
-            </div>
-            </div>
+                                    <Paper elevation={3} style={{
+                                        padding: 8,
+                                        border: "1px solid black",
+                                        justifyContent: "center",
+                                        alignItems: "center",
+                                        textAlign: "center",
+                                        verticalAlign: "middle",
+                                        height: "100%"
+                                    }}>
+                                        <h4 className="addType">
+                                            {ad.Type}
+                                        </h4>
+                                        <div className="paperTitleAndDate">
+                                            <h1>
+                                                {ad.Title}
+                                            </h1>
+                                            <h4>
+                                                Dato opprettet: {new Date(ad.Created * 1000).toString().slice(3, 10)} 2023
+                                            </h4>
+                                        </div>
+                                        <h3>
+                                            {ad.Description}
+                                        </h3>
+                                        <div className="advertPaperButtons">
+                                            <button onClick={async () => {
+                                                const adDoc = doc(firestore, "Advertisement", ad.id);
+                                                await deleteDoc(adDoc);
+                                                getAds();
+                                            }}>Slett annonse</button>
+                                        </div>
+                                    </Paper>
+                                </Box>
+                            ))}
+                        </List>
+                    </div>
+                </div>
             </Container>
         </div>
 
 
-        
+
     );
 }
 
